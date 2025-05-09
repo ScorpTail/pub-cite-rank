3278be2e4187b5e7b23e6c1698372382c8077a31
Danchevskyi Vladyslav <scorpio232354@gmail.com>
2025-05-09 22:58:23 +0300

    feat: create Author model and migration for authors table

---
+++ app/Models/Author.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Author extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'authors';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'first_name',
+        'middle_name',
+        'last_name',
+        'birth_date',
+        'about',
+        'affiliation',
+        'openalex_id',
+    ];
+
+    /**
+     * Get the user that owns the Author
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id', 'id');
+    }
+
+    public function getFullNameAttribute()
+    {
+        return trim("{$this->first_name} {$this->middle_name} {$this->last_name}");
+    }
+}
