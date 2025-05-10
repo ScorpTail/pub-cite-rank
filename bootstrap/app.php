25076628794dccc041aa18c11853e4423171d8f9
Danchevskyi Vladyslav <scorpio232354@gmail.com>
2025-05-10 14:33:29 +0300

    feat: enhance middleware configuration by adding aliases and stateful API support

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,18 +1,29 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\AdminMiddleware;
+use App\Http\Middleware\GuestMiddleware;
+use App\Http\Middleware\OptionalAuthMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
+        web: __DIR__ . '/../routes/web.php',
+        api: __DIR__ . '/../routes/api.php',
+        commands: __DIR__ . '/../routes/console.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+
+        $middleware->alias([
+            'admin' => AdminMiddleware::class,
+            'guest' => GuestMiddleware::class,
+            'auth.optional' => OptionalAuthMiddleware::class,
+        ]);
+        $middleware->statefulApi();
+
+        $middleware->appendToGroup('api', [OptionalAuthMiddleware::class]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
