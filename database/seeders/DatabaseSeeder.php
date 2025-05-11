328ed91aba3cd7f6dbf776e877fcc2f3eec1c09c
Danchevskyi Vladyslav <scorpio232354@gmail.com>
2025-05-11 23:24:58 +0300

    refactor: simplify permission creation in DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,111 +26,46 @@ class DatabaseSeeder extends Seeder
 
         DB::transaction(function () {
             $permissions = [
-                [
-                    'name' => 'role_create',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'role_edit',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'permission_edit',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'user_create',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'user_edit',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'publish_create',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'publish_edit',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'author_create',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'author_edit',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'journal_create',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'journal_edit',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'category_create',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'category_edit',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
-                [
-                    'name' => 'admin_panel',
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ],
+                'role_create',
+                'role_edit',
+                'permission_edit',
+                'user_create',
+                'user_edit',
+                'publish_create',
+                'publish_edit',
+                'author_create',
+                'author_edit',
+                'journal_create',
+                'journal_edit',
+                'category_create',
+                'category_edit',
+                'weight_create',
+                'weight_edit',
+                'admin_panel',
             ];
 
-            $permissions = Permission::all()->filter(function ($permission) use ($permissions) {
-                return !in_array($permission->name, array_column($permissions, 'name'));
-            })->map(function ($permission) use ($permissions) {
-                $permissions[] = [
-                    'name' => $permission->name,
-                    'guard_name' => 'api',
-                    'created_at' => now(),
-                    'updated_at' => now(),
-                ];
-            })->toArray();
+            $existsPermissions = Permission::get('name')->pluck('name');
 
-            Permission::insert($permissions);
+            $createPermissions = [];
+            foreach ($permissions as $permission) {
+                if (!$existsPermissions->contains($permission)) {
+                    $createPermissions[] = [
+                        'name' => $permission,
+                        'guard_name' => 'api',
+                        'created_at' => now()->format('Y-m-d H:i:s'),
+                        'updated_at' => now()->format('Y-m-d H:i:s'),
+                    ];
+                }
+            }
+
+            Permission::insert($createPermissions);
 
             if (!DB::table('roles')->where('name', 'admin')->exists()) {
                 DB::table('roles')->insert(['name' => 'admin', 'guard_name' => 'api', 'created_at' => now(), 'updated_at' => now()]);
             }
 
             $admin = DB::table('roles')->where('name', 'admin')->first();
-            DB::table('permissions')->whereIn('name', data_get($permissions, '*.name'))->get()->each(function ($permission) use ($admin) {
+            DB::table('permissions')->whereIn('name', data_get($createPermissions, '*.name'))->get()->each(function ($permission) use ($admin) {
                 DB::table('role_has_permissions')->insert(['permission_id' => $permission->id, 'role_id' => $admin->id]);
             });
             User::find(1)->assignRole('admin');
