add6976df22da2d749f13402b1c93dfce6748b46
Danchevskyi Vladyslav <scorpio232354@gmail.com>
2025-05-17 22:04:45 +0300

    feat: customize PublicationResource to return detailed publication data

--- app/Http/Resources/Publication/PublicationResource.php
+++ app/Http/Resources/Publication/PublicationResource.php
@@ -14,6 +14,28 @@ class PublicationResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        return parent::toArray($request);
+        $code = strtolower($this->publisher->country);
+        return [
+            'title' => $this->title,
+            'authors' => $this->authors()->orderBy('author_position', 'asc')->get()->map(function ($author) {
+                return [
+                    'id' => $author->id,
+                    'name' => $author->full_name,
+                ];
+            }),
+            'categories' => $this->categories()->get()->map(function ($category) {
+                return [
+                    'id' => $category->id,
+                    'name' => $category->name,
+                ];
+            }),
+            'published_at' => $this->published_at,
+            'publisher_id' => $this->publisher_id,
+            'publisher_name' => $this->publisher->name,
+            'publisher_country' => "https://flagcdn.com/48x36/{$code}.png",
+            'citation_count' => $this->citation_count,
+            'doi' => $this->doi,
+            'openalex_id' => 'https://openalex.org/concepts/C' . $this->openalex_id,
+        ];
     }
 }
