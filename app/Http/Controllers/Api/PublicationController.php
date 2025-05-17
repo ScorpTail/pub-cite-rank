519f51fdf5a3b053d1e836c71f43c4b281f62feb
Danchevskyi Vladyslav <scorpio232354@gmail.com>
2025-05-17 16:52:52 +0300

    refactor: reorganize PublicationController methods and improve index functionality for filtering publications

--- app/Http/Controllers/Api/PublicationController.php
+++ app/Http/Controllers/Api/PublicationController.php
@@ -2,48 +2,46 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Publication;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\Http\Resources\Publication\PublicationResource;
 
 class PublicationController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        //
-    }
+        $query = Publication::query();
 
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
+        if ($request->has('published_at')) {
+            $query->where('published_at', '>=', $request->input('published_at'));
+        }
+
+        if ($request->has('category_id')) {
+            $query->whereHas('categories', function ($q) use ($request) {
+                $q->where('id', $request->input('category_id'));
+            });
+        }
+
+        if ($request->has('publisher_id')) {
+            $query->where('publisher_id', $request->input('publisher_id'));
+        }
+
+        $publications = $query->paginate(15);
+
+        return PublicationResource::collection($publications);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $publicationId)
     {
-        //
-    }
+        $publication = Publication::findOrFail($publicationId);
 
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, string $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(string $id)
-    {
-        //
+        return PublicationResource::make($publication);
     }
 }
